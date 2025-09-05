eba945db3b78597ec2fe4b92844dc34428332e17
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-09-05 12:28:14 -0300

    feat: Update Config

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 import { ApiConfigService } from '../../service/api-config.service';
-import { Observable, tap } from 'rxjs';
+import { delay, Observable, tap } from 'rxjs';
+import { MoedaTableComponent } from "../../component/moeda-table/moeda-table.component";
+import { Cotacao } from '../../interfaces/cotacao';
+import { MoedaConversorComponent } from "../../component/moeda-conversor/moeda-conversor.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MoedaTableComponent, MoedaConversorComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  lista: string[] = [];
+  lista: Cotacao[] | null = null;
 
-  constructor(private service:ApiConfigService){}
+  constructor(private service:ApiConfigService){
+    delay(2000);
+    this.getListaDeCotacoes();
+  }
 
-  getMoeda() {
-    this.service.buscarMoedasSuportadas().pipe(
+  getListaDeCotacoes() {
+    this.service.buscarListaDeCotacoes().pipe(
       tap((moedas) => (this.lista = moedas, console.log(this.lista))),
     ).subscribe();
   }
