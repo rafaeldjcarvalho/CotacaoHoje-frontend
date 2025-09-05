2dad8b116e933cedf5927a491aa002748f37b317
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-09-05 14:43:09 -0300

    feat: Update Configs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
+import { HistoricoCotacoesComponent } from './pages/historico-cotacoes/historico-cotacoes.component';
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {path: "", pathMatch: "full", redirectTo: "dashboard"},
-  {path: "**", pathMatch: "full", redirectTo: "dashboard"},
   {
     path: "dashboard",
     component: DashboardComponent
-  }
+  },
+  {
+    path: "historico",
+    component: HistoricoCotacoesComponent
+  },
+  {path: "**", pathMatch: "full", redirectTo: "dashboard"}
 ];
